8eb4f03e14cf7d302937c36c7fb51fdd77e2e5dd
EluneyM <eluneymerillan@gmail.com>
2019-04-21 17:11:31 -0300

    Volviendo a definir la estructura

--- paginaJuego.php
+++ paginaJuego.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Batalla Naval</title>
+    <title>Bootstrap 4, from LayoutIt!</title>
 
     <meta name="description" content="Source code generated using layoutit.com">
     <meta name="author" content="LayoutIt!">
@@ -20,157 +20,150 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h3>
-				Batalla Naval
+				h3. Lorem ipsum dolor sit amet.
 			</h3>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<ul class="nav">
-				<li class="nav-item">
-					<a class="nav-link active" href="#">Home</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Profile</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#">Messages</a>
-				</li>
-				<li class="nav-item dropdown ml-md-auto">
-					 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-						 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-						<div class="dropdown-divider">
-						</div> <a class="dropdown-item" href="#">Separated link</a>
-					</div>
-				</li>
-			</ul>
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-6">
-					<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
-				</div>
-				<div class="col-md-6">
-					<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
-				</div>
-			</div>
+		<div class="col-md-6">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>
+							#
+						</th>
+						<th>
+							Product
+						</th>
+						<th>
+							Payment Taken
+						</th>
+						<th>
+							Status
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							1
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							01/04/2012
+						</td>
+						<td>
+							Default
+						</td>
+					</tr>
+					<tr class="table-active">
+						<td>
+							1
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							01/04/2012
+						</td>
+						<td>
+							Approved
+						</td>
+					</tr>
+					<tr class="table-success">
+						<td>
+							2
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							02/04/2012
+						</td>
+						<td>
+							Declined
+						</td>
+					</tr>
+					<tr class="table-warning">
+						<td>
+							3
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							03/04/2012
+						</td>
+						<td>
+							Pending
+						</td>
+					</tr>
+					<tr class="table-danger">
+						<td>
+							4
+						</td>
+						<td>
+							TB - Monthly
+						</td>
+						<td>
+							04/04/2012
+						</td>
+						<td>
+							Call in to confirm
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="col-md-6">
+			<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-6">
-					<table class="table">
-						<thead>
-							<tr>
-								<th>
-									#
-								</th>
-								<th>
-									Barcos
-								</th>
-								<th>
-									Coordenadas
-								</th>
-								<th>
-									Orientación
-								</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>
-									1
-								</td>
-								<td>
-									Acorazado
-								</td>
-								<td>
-									A - 10
-								</td>
-								<td>
-									horizontal
-								</td>
-							</tr>
-							<tr class="table-active">
-								<td>
-									2
-								</td>
-								<td>
-									Destructor
-								</td>
-								<td>
-									B - 2
-								</td>
-								<td>
-									vertical
-								</td>
-							</tr>
-							<tr class="table-success">
-								<td>
-									3
-								</td>
-								<td>
-									Portaaviones
-								</td>
-								<td>
-									E - 5
-								</td>
-								<td>
-									Vertical
-								</td>
-							</tr>
-							<tr class="table-warning">
-								<td>
-									4
-								</td>
-								<td>
-									Corbeta
-								</td>
-								<td>
-									B - 7
-								</td>
-								<td>
-									Horizontal
-								</td>
-							</tr>
-							<tr class="table-danger">
-								<td>
-									5
-								</td>
-								<td>
-									Cañonera
-								</td>
-								<td>
-									J - 3
-								</td>
-								<td>
-									Horizontal
-								</td>
-							</tr>
-						</tbody>
-					</table>
+		<div class="col-md-6">
+			<form role="form">
+				<div class="form-group">
+					 
+					<label for="exampleInputEmail1">
+						Email address
+					</label>
+					<input type="email" class="form-control" id="exampleInputEmail1">
 				</div>
-				<div class="col-md-6">
-					<form role="form">
-						<div class="form-group">
-							 
-							<label for="exampleInputEmail1">
-								Coordenadas
-							</label>
-							<input type="email" class="form-control" id="exampleInputEmail1">
-						</div>
-						<div class="checkbox">
-						</div> 
-						<button type="submit" class="btn btn-primary">
-							Disparar
-						</button>
-					</form>
+				<div class="form-group">
+					 
+					<label for="exampleInputPassword1">
+						Password
+					</label>
+					<input type="password" class="form-control" id="exampleInputPassword1">
 				</div>
-			</div>
+				<div class="form-group">
+					 
+					<label for="exampleInputFile">
+						File input
+					</label>
+					<input type="file" class="form-control-file" id="exampleInputFile">
+					<p class="help-block">
+						Example block-level help text here.
+					</p>
+				</div>
+				<div class="checkbox">
+					 
+					<label>
+						<input type="checkbox"> Check me out
+					</label>
+				</div> 
+				<button type="submit" class="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		</div>
+		<div class="col-md-6">
 		</div>
 	</div>
 </div>
